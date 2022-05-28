2d4d1054834f74b518a876d4118112593493c2e7
Arsen <abdigal.2015@gmail.com>
2022-05-28 22:44:10 +0600

    added supportsinterface function

--- contracts/tokens/MyERC1155.sol
+++ contracts/tokens/MyERC1155.sol
@@ -2,36 +2,26 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
-import "@openzeppelin/contracts/utils/Counters.sol";
 
 contract MyERC1155 is ERC1155 {
-    using Counters for Counters.Counter;
-    
-    Counters.Counter private currentTokenId;
-
     string private _name;
     string private _symbol;
 
-    constructor(string memory name_, string memory symbol_, string memory uri_)
-    ERC1155(uri_)
-    {
+    constructor(
+        string memory name_,
+        string memory symbol_,
+        string memory uri_
+    ) ERC1155(uri_) {
         _name = name_;
         _symbol = symbol_;
     }
 
-    function mintTo(address account, uint256 amount) public returns(uint) {
-        currentTokenId.increment();
-        uint id = currentTokenId.current();
-        _mint(account, id, amount, "");
-        emit TransferSingle(address(0), address(0), account, id, amount);
-        return id;
-    }
-
-    function mintBatch(address account, uint256[] memory amounts) public {
-        for (uint i = 0; i < amounts.length; i++) {
-            currentTokenId.increment();
-            _balances[currentTokenId.current()][account] = amounts[i];
-        }
+    function mintTo(
+        address account,
+        uint256 amount,
+        uint256 tokenId
+    ) public {
+        _mint(account, tokenId, amount, "");
     }
 
     function name() public view returns (string memory) {
@@ -40,5 +30,15 @@ contract MyERC1155 is ERC1155 {
 
     function symbol() public view returns (string memory) {
         return _symbol;
+    }
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        virtual
+        override
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
     }
 }
--- contracts/tokens/MyERC721.sol
+++ contracts/tokens/MyERC721.sol
@@ -2,32 +2,37 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/utils/Counters.sol";
 
 contract MyERC721 is ERC721 {
-    using Counters for Counters.Counter;
     string private _uri;
-    
-    Counters.Counter private currentTokenId;
 
-    constructor(string memory name_, string memory symbol_, string memory uri_)
-    ERC721(name_, symbol_) {
+    constructor(
+        string memory name_,
+        string memory symbol_,
+        string memory uri_
+    ) ERC721(name_, symbol_) {
         setUri(uri_);
     }
 
-    function mintTo(address receiver) public returns(uint) {
-        currentTokenId.increment();
-        uint id = currentTokenId.current();
-        _safeMint(receiver, id);
-        emit Transfer(address(0), receiver, id);
-        return id;
+    function mintTo(address to, uint256 tokenId) public {
+        _mint(to, tokenId);
     }
-    
-    function _baseURI() internal override view virtual returns (string memory) {
+
+    function _baseURI() internal view virtual override returns (string memory) {
         return _uri;
     }
-    
+
     function setUri(string memory uri) internal {
         _uri = uri;
+    }
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        virtual
+        override
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
     }
 }
