2ac01cd5a22f3170d56018200e0f5621114676e4
Arsen <abdigal.2015@gmail.com>
2022-05-28 12:11:44 +0600

    deployed ERC1155

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -4,9 +4,14 @@ async function main() {
   const ERC721 = await ethers.getContractFactory("MyERC721");
   const erc721 = await ERC721.deploy("NFT", "NFT");
 
+  const ERC1155 = await ethers.getContractFactory("MyERC1155");
+  const erc1155 = await ERC1155.deploy();
+
   await erc721.deployed();
+  await erc1155.deployed();
 
   console.log("ERC721 deployed to:", erc721.address);
+  console.log("ERC1155 deployed to", erc1155.address);
 }
 
 main().catch((error) => {
